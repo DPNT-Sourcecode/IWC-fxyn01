7ce7550c82ce1344ea437065cf974e76a7743b91
sbx_user1051 <sbx_user1051@169.254.3.77>
2026-01-20 15:30:09 +0000

    Tue Jan 20 15:30:09 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -22,11 +22,11 @@ def test_rule_of_three_applies() -> None:
     run_queue([
         call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(1),
         call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(2),
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(3),
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(4),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(3),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(4),
         call_dequeue().expect("companies_house", 1),
-        call_dequeue().expect("companies_house", 1),
-        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("id_verification", 1),
         call_dequeue().expect("companies_house", 2)
     ])
 
