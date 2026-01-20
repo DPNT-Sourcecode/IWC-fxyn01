ca602be8065b7cb6a27b454b024bd444be878c9a
sbx_user1051 <sbx_user1051@169.254.3.77>
2026-01-20 16:04:15 +0000

    Tue Jan 20 16:04:15 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -282,9 +282,3 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
-
-
-
-
-
-
--- lib/solutions/IWC/task_types.py
+++ lib/solutions/IWC/task_types.py
@@ -24,5 +24,3 @@ class TaskDispatch:
 
 
 __all__ = ["TaskSubmission", "TaskDispatch"]
-
-
