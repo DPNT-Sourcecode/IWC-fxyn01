42fb187c118759fc86d12fe8c7d4bb11932d1203
sbx_user1051 <sbx_user1051@169.254.92.145>
2026-01-20 09:56:43 +0000

    Tue Jan 20 09:56:43 UTC 2026

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -1,6 +1,9 @@
-from solutions.SUM.sum_solution import SumSolution
+from lib.solutions.SUM.sum_solution import SumSolution
 
 
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+
+    def test_larger_number(self):
+        assert SumSolution().compute(100008, 200009) == 300017
