7fed46fff16f1d705acd5ba66cb8d3f5a216a201
sbx_user1051 <sbx_user1051@169.254.3.77>
2026-01-20 16:53:06 +0000

    Tue Jan 20 16:53:06 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -117,9 +117,9 @@ def test_multiple_bank_statements_timestamp_prioritised_correctly() -> None:
         call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(2),
         call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(3),
-        call_enqueue("credit_check", 2, iso_ts(delta_minutes=0)).expect(4),
+        call_enqueue("id_verification", 2, iso_ts(delta_minutes=0)).expect(4),
         call_dequeue().expect("companies_house", 1),
-        call_dequeue().expect("credit_check", 1),
+        call_dequeue().expect("id_verification", 2),
         call_dequeue().expect("bank_statements", 2),
         call_dequeue().expect("bank_statements", 1)
     ])
