84a43011a51f6ba7b73726819589dd35603b86dd
sbx_user1051 <sbx_user1051@169.254.16.133>
2026-01-21 17:10:19 +0000

    Wed Jan 21 17:10:19 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
-# from functools import cmp_to_key
+from functools import cmp_to_key
 
 # LEGACY CODE ASSET
 # RESOLVED on deploy
@@ -189,8 +189,7 @@ class Queue:
             key=lambda i: (
                 self._priority_for_task(i),
                 self._earliest_group_timestamp_for_task(i),
-                self._deprioritise_bank_statements(i),
-                self._timestamp_for_task(i)
+                self._deprioritise_bank_statements(i)
             )
         )
 
@@ -220,7 +219,7 @@ class Queue:
         # as expected alongside timestamps. I have left this for completeness.
         # self._queue.sort(key=cmp_to_key(self._prioritise_older_bank_statements))
 
-
+        self._queue.sort(key=lambda i: self._timestamp_for_task(i))
 
         task = self._queue.pop(0)
 
@@ -388,8 +387,3 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
-
-
-
-
-
--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -205,7 +205,18 @@ def test_old_bank_statements_occur_before_other_events_multiple_events_together_
 
 def test_old_bank_statements_occur_after_earlier_timestamps() -> None:
     run_queue([
-        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=7)).expect(1),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=2)).expect(1),
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=3)).expect(2),
+        call_enqueue("id_verification", 2, iso_ts(delta_minutes=7)).expect(3),
+
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("id_verification", 2),
+    ])
+
+def test_old_bank_statements_occur_after_earlier_timestamps_copy() -> None:
+    run_queue([
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
         call_enqueue("companies_house", 1, iso_ts(delta_minutes=3)).expect(2),
         call_enqueue("id_verification", 2, iso_ts(delta_minutes=2)).expect(3),
 
@@ -216,14 +227,14 @@ def test_old_bank_statements_occur_after_earlier_timestamps() -> None:
 
 def test_old_bank_statements_occur_before_new_event_but_after_one_with_less_than_required_age() -> None:
     run_queue([
-        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=7)).expect(1),
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=7)).expect(2),
         call_enqueue("id_verification", 2, iso_ts(delta_minutes=2)).expect(3),
         call_enqueue("companies_house", 2, iso_ts(delta_minutes=4)).expect(4),
         call_dequeue().expect("bank_statements", 1),
-        call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("id_verification", 2),
         call_dequeue().expect("companies_house", 2),
+        call_dequeue().expect("companies_house", 1)
     ])
 
 def test_multiple_bank_statements_rule_of_three_prioritised_below_old_bank_statements() -> None:
@@ -237,5 +248,3 @@ def test_multiple_bank_statements_rule_of_three_prioritised_below_old_bank_state
         call_dequeue().expect("credit_check", 1),
         call_dequeue().expect("bank_statements", 1),
     ])
-
-
