e86bcdc8ac109f320d4cfb4eac81772410f7d1ec
sbx_user1051 <sbx_user1051@169.254.16.133>
2026-01-21 17:57:39 +0000

    Wed Jan 21 17:57:39 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -249,6 +249,17 @@ def test_multiple_bank_statements_rule_of_three_prioritised_below_old_bank_state
         call_dequeue().expect("bank_statements", 1),
     ])
 
-def test_
+def test_bank_statement_shifts_only_above_non_bank_statements_with_same_timestamp() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=5)).expect(1),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(3),
+        call_enqueue("credit_check", 1, iso_ts(delta_minutes=6)).expect(4),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("bank_statements", 2),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("credit_check", 1),
+
+    ])
 
 
